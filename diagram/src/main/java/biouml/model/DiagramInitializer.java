0a9e7f321c745421ae16c7f06b660d8a67125b5b
Ilya <damagedone@yandex.ru>
2025-11-29 17:48:51 +0400

    not compilable code commented

--- diagram/src/main/java/biouml/model/DiagramInitializer.java
+++ diagram/src/main/java/biouml/model/DiagramInitializer.java
@@ -6,7 +6,7 @@ import biouml.model.server.DiagramService;
 import biouml.model.server.WebDiagramsActionsProvider;
 import biouml.model.server.WebDiagramsProvider;
 import biouml.plugins.server.access.AccessService;
-import ru.biosoft.access.DataElementImporterRegistry;
+//import ru.biosoft.access.DataElementImporterRegistry;
 import ru.biosoft.access.file.FileType;
 import ru.biosoft.access.file.FileTypePriority;
 import ru.biosoft.access.file.FileTypeRegistry;
@@ -89,6 +89,7 @@ public class DiagramInitializer extends Initializer
     @Override
     protected void initImporters()
     {
-        DataElementImporterRegistry.registerImporter( "DMLImporter", "biouml.model.util.DiagramDMLImporter", "BioUML format(*.dml)", null );
+    	AccessService.addCommonClass( "biouml.model.Diagram" );
+//        DataElementImporterRegistry.registerImporter( "DMLImporter", "biouml.model.util.DiagramDMLImporter", "BioUML format(*.dml)", null );
     }
 }
